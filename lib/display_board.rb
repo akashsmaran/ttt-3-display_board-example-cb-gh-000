8d14fc690f53a226afed93525f0d5ccbad319b8c
AKASH  SMARAN <akashsmaran@gmail.com>
2018-01-05 15:39:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,9 @@ def display_board(board_state)
 puts"Welcome to Tic Tac Toe"
 puts " "+board_state[0]+" | "+board_state[1]+" | "+ board_state[2]
 puts "-----------"
-
+puts " "+board_state[3]+" | "+board_state[4]+" | "+ board_state[5]
+puts "-----------"
+puts " "+board_state[6]+" | "+board_state[7]+" | "+ board_state[8]
+puts "-----------"
 end
 display_board(board_state)
