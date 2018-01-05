ed20a00638652a05da3c8ae92fcaa3a8be481962
AKASH  SMARAN <akashsmaran@gmail.com>
2018-01-05 15:37:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 board_state = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
+def display_board(board_state)
 puts"Welcome to Tic Tac Toe"
 puts board_state[0]+" |"+board_state[1]+" |"+ board_state[2]
 puts "-----------"
 
 end
-display_board
+display_board(board_state)
