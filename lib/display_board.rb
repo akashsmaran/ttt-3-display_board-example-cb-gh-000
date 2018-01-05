6cf681403063502b673c7bdbdac7ef0102fbe2d6
AKASH  SMARAN <akashsmaran@gmail.com>
2018-01-05 15:45:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@ board_state = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board_state)
 puts"Welcome to Tic Tac Toe"
-puts " "+board_state[0]+" | "+board_state[1]+" | "+ board_state[2]
+puts "   "+"|"+"   "+"|"+"   "
 puts "-----------"
-puts " "+board_state[3]+" | "+board_state[4]+" | "+ board_state[5]
+puts "   "+"|"+"   "+"|"+"   "
 puts "-----------"
-puts " "+board_state[6]+" | "+board_state[7]+" | "+ board_state[8]
+puts "   "+"|"+"   "+"|"+"   "
 end
-display_board(board_state)
+display_board()
